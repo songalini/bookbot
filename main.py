b6647a37dfaabf7d437551ed60eafb05ae27bcee
songalini <songalinidev@gmail.com>
2024-12-20 16:26:58 +1100

    C: ordered by character amount

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ def main():
     book_path = "books/frankenstein.txt" 
     words = get_text(book_path)
     num_words = word_count(words)
-    lower = lowercase_words(words)
     num_chars = characters_total(words)
-    character_dictionary = character_list(words)
-    print(f"--- Content Summary of {book_path} ---" )
+    character_dictionary_alpha = character_dict_alpha(words)
+    character_dict_num = character_dict_numerical(character_dictionary_alpha)
+    print(f"--- Content Summary of {book_path} --- \n")
     print(f"{num_words} words were found, with a total of {num_chars} letters ")
-    for i in character_dictionary:
-        print(f"Character '{i}' was found {character_dictionary[i]} times!")
-
+    for char, amount in character_dict_num.items():
+        print(f"Character '{char}' was found {amount} times!")
+    print(f" --- End of Content Summary ---")
+    
 def get_text(path):
     with open(path) as f:
         return f.read()
@@ -26,9 +27,8 @@ def lowercase_words(words):
             lowercase_words.append(lowercase[i])
     return lowercase_words 
 
-def character_list(words):
-    character_dict = {
-    }
+def character_dict_alpha(words):
+    character_dict = {}
     character_list = list(lowercase_words(words))
     character_list.sort()
     for i in range(len(character_list))[1::]:
@@ -37,6 +37,14 @@ def character_list(words):
         else:
             character_dict[(character_list[i])] = 1
     return character_dict
+    
+def character_dict_numerical(character_dictionary_alpha):
+    character_dict_num = {}
+    for k, v in sorted(character_dictionary_alpha.items(), key=lambda item:item[1], reverse=True):
+        character_dict_num[k] = v
+    return character_dict_num
+    
+    
 
 def characters_total(words):
     character_list = list(lowercase_words(words))
