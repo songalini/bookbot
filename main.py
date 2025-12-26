804cdd8d5f5558d3d2c55d8b9953372693b39041
songalini <songalinidev@gmail.com>
2025-12-27 01:39:10 +1100

    D: added comments to main.py explaining lines

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from stats import get_word_count, get_char_count, build_char_report
-import sys
+from stats import get_word_count, get_char_count, build_char_report                 #import functions from stats.py
+import sys                                                                          #import sys
 
 def main():
-    if len(sys.argv) <2:
-        print("Usage: python3 main.py <path_to_book>")
-        # print("Example: 'python main.py books/frankenstein.txt'")
-        # print("Exiting...")
-        sys.exit(1)
-    path = sys.argv[1]
+    #main program function
+    if len(sys.argv) <2:                                            #if len of sys.argv is <2, aka you're not using the program correctly  
+        print("Usage: python3 main.py <path_to_book>")              #explain how to use it
+        print("Example: 'python main.py books/frankenstein.txt'")   
+        print("Exiting...")
+        sys.exit(1)                                                 #exits the program with the error code 1
+
+    path = sys.argv[1]                                              #path variable set to sys.argv[1], aka the term for the file location
     
-    report = (
+    report = (                                                      #prints the report, calling functions from stats.py
         f"============ BOOKBOT ============\n"
         f"\n"
         f"Analyzing book found at {path}...\n"
